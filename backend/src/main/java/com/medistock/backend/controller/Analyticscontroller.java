f66fd569d730f4b25966a62b88540a9d60180255
Sri Varshini Vuchuru <srivarshinivuchuru@gmail.com>
2026-08-17 15:11:00 +0530

    Comment out CrossOrigin annotation in Analyticscontroller
    Comment out the CrossOrigin annotation in Analyticscontroller.

--- backend/src/main/java/com/medistock/backend/controller/Analyticscontroller.java
+++ backend/src/main/java/com/medistock/backend/controller/Analyticscontroller.java
@@ -2,7 +2,7 @@ package com.medistock.backend.controller;
 
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/analytics")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class Analyticscontroller {
 
     private final AnalyticsService analyticsService;
